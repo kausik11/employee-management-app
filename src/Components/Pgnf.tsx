0654ff3a9c6228fb4e3a6adeb0e5fa8d745bd8b1
kausik <kkausik11@gmail.com>
2025-03-01 18:47:04 +0530

    final commit fixed page not found

--- src/Components/Pgnf.tsx
+++ src/Components/Pgnf.tsx
@@ -3,7 +3,7 @@
 const pgnf = () => {
   return (
     <div  className="w-full max-w-4xl mx-auto p-4">
-      <img src="../../public/pgnf.jpg"  className="w-full h-auto rounded-2xl object-cover"></img>
+      <img src="/pgnf.jpg"  className="w-full h-auto rounded-2xl object-cover"></img>
     </div>
   )
 }
